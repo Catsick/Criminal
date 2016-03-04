16467744259fb66ae64833c9332af58ca44fd79f
Bonnie <bmr28@wildcats.unh.edu>
2016-03-03 20:26:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bonnie/crimeactivity/Crime.java
@@ -0,0 +1,29 @@
+package com.example.bonnie.crimeactivity;
+
+import java.util.UUID;
+
+/**
+ * Created by Bonnie on 3/3/2016.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime () {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+
+}
